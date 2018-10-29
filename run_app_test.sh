9a02e63ef21d5cfb79cd9048a0e4b54000ae2a95
rishabghosh <44020133+rishabghosh@users.noreply.github.com>
2018-10-29 19:06:45 +0530

    run_app_test.sh highlights overall tests result at the bottom.
    now its easy to read the tests result
    dont have to look up and down to see if anycode in middle has broken.
    
    prev commit was done mistakenly.

--- run_app_test.sh
+++ run_app_test.sh
@@ -4,8 +4,11 @@
 ./run_test.sh createTriangle.js appTestData/inputForTriangle.txt appTestData/outputForTriangle.txt
 ./run_test.sh createDiamond.js appTestData/inputForDiamond.txt appTestData/outputForDiamonds.txt
 
-
+echo "--------------"
+echo "tests for rectangle "
 ./run_test.sh createRectangle.js appTestData/inputForRectangles.txt appTestData/outputForRectangles.txt | tail -1
+echo "tests for triangles "
 ./run_test.sh createTriangle.js appTestData/inputForTriangle.txt appTestData/outputForTriangle.txt | tail -1
-
+echo "tests for diamonds "
+./run_test.sh createDiamond.js appTestData/inputForDiamond.txt appTestData/outputForDiamonds.txt | tail -1
 
