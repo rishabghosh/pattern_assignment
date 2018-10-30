53a4a41344bb1098137a4f072054c5574e25e21c
rishabghosh <44020133+rishabghosh@users.noreply.github.com>
2018-10-30 10:50:37 +0530

    run_app_test.sh is moved to scripts directory
    also it is modified to pass successfully
    run_test.sh is also moved to scripts directory

---
+++ scripts/run_app_test.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+./run_test.sh ../createRectangle.js ../appTestData/inputForRectangles.txt ../appTestData/outputForRectangles.txt
+./run_test.sh ../createTriangle.js ../appTestData/inputForTriangle.txt ../appTestData/outputForTriangle.txt
+./run_test.sh ../createDiamond.js ../appTestData/inputForDiamond.txt ../appTestData/outputForDiamonds.txt
+
